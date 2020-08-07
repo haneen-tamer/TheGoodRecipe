a4018ddc370b301a279de49ce1d6cb6be433727a
andrewgadelkarim <andrew.amgad1@gmail.com>
2020-08-07 18:52:47 +0200

    new comment
    comment by me

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace TheGoodRecipe
         [STAThread]
         static void Main()
         {//hi
+            //hi(andrew)
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
