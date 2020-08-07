74c8f7ffc73eedbd1932a165a58ac1b35777bac9
andrewgadelkarim <andrew.amgad1@gmail.com>
2020-08-07 21:43:40 +0200

    creation of classes: user, ingredient, recipe

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace TheGoodRecipe
         {//hi
             //hi(andrew)
             //hello
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
---
+++ User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGoodRecipe
+{
+    class User
+    {
+        #region Variables
+        private String userName;
+        private String name;
+        private String job;
+        private List<int> favouriteRecipesIDs;
+        #endregion
+
+        #region Constructors
+        public User()
+        {
+            this.userName = "";
+            this.name = "";
+            this.job = "";
+            this.favouriteRecipesIDs = new List<int>();
+        }
+        public User(String userName, String name, String job, List<int> favouriteRecipesIDs)
+        {
+            this.userName = userName;
+            this.name = name;
+            this.job = job;
+            this.favouriteRecipesIDs = favouriteRecipesIDs;
+        }
+        #endregion
+
+        #region Getters and Setters
+        public string UserName { get => userName; set => userName = value; }
+        public string Name { get => name; set => name = value; }
+        public string Job { get => job; set => job = value; }
+        public List<int> FavouriteRecipesIDs { get => favouriteRecipesIDs; set => favouriteRecipesIDs = value; }
+        #endregion
+    }
+}
