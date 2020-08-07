da5ebfc90dd48ee1a0fb0702291d909603ef1115
Haneen Tamer <haneen.tamer@cis.asu.edu.eg>
2020-08-07 21:03:16 +0200

    User Classes

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace TheGoodRecipe
         /// </summary>
         [STAThread]
         static void Main()
-        {//hi
+        {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
---
+++ UserState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGoodRecipe
+{
+    abstract class UserState
+    {
+        protected UserManager um;
+        public abstract void login(string username, string pass);
+        public abstract void signUp(string username, string name, string pass);
+        public abstract void logout();
+        public abstract void postReview();
+    }
+}
